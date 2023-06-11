20f03e708d261f60f42cd9e04bdd94b159220055
Bartek <bartlomiejrzepka@wp.pl>
2023-06-11 13:34:59 +0200

    feat: implement initial chat component [MAR-5]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { DateTime } from "luxon";
+import { ChatData } from "./components/chat/chat.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  chatData: ChatData = {
+    senderName: 'John Doe',
+    senderAvatar: '',
+    userAvatar: '',
+    messages: [
+      {
+        sentDateTime: DateTime.now().minus({ minute: 5 }),
+        content: 'Hello, whats up?',
+        sentBy: 'SENDER'
+      },
+      {
+        sentDateTime: DateTime.now().minus({ minute: 2 }),
+        content: 'I am enjoying leisure time with my family, thanks!',
+        sentBy: 'USER'
+      },
+      {
+        sentDateTime: DateTime.now().minus({ minute: 1, second: 50 }),
+        content: 'What about you?',
+        sentBy: 'USER'
+      }
+    ]
+  }
+}
